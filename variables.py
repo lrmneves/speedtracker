49453fc657f4a7ca4a40e9c89b3d4722ea63a3e7
Leonardo <lrmneves@poli.ufrj.br>
2015-06-12 00:20:36 -0300

    Added defaut init parameters for videos 2 and 3

--- variables.py
+++ variables.py
@@ -6,16 +6,25 @@ video = "videos/video1.mov"
 
 
 
-
+#video1
 areaOfInterest = [(300,250),(800,630)] 
 #video2
-# areaOfInterest = [(100,150),(450,200)] 
+# areaOfInterest = [(100,100),(250,200)] 
+#video3
+# areaOfInterest = [(100,150),(250,250)] 
+
 x0 = areaOfInterest[0][0]
 x1 = areaOfInterest[1][0]
 y0 = areaOfInterest[0][1]
 y1 = areaOfInterest[1][1]
 #define in meter the aprox width of the area
+#video1
 areaOfInterestWidth = 8
+#video 2
+# areaOfInterestWidth = 4
+#video 3
+# areaOfInterestWidth = 4
+
 #direction of the cars
 LEFT_TO_RIGHT = "leftToRight"
 RIGHT_TO_LEFT = "rightToLeft"
